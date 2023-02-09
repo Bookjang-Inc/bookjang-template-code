79f5d175b6bc06e7519bd0e8a704384560c2cfd6
EunwooSong <song200348@gmail.com>
2023-02-09 16:55:17 +0900

    Add Course03 Part02 template code

---
+++ course03/IV_10_1.py
@@ -0,0 +1,10 @@
+#정수를 두 개 입력받으세요.
+
+
+# 연산 결과를 변수에 대입합니다.
+# (addition, subtraction, multiplication, division, quotient, remainder)
+
+
+# print function을 이용해서 연산 결과를 출력합니다.
+# print(a, '+', b, '=', addition)
+
